fa8100cb3264db232fe18b7bee9607c6bb6d2653
kangnayeon <seoultownroad@naver.com>
2024-12-01 15:05:08 +0900

    day-1-b

--- day-1/b.ts
+++ day-1/b.ts
@@ -2,8 +2,31 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day1b(data: string[]) {
-  console.log(data);
-  return 0;
+  const left = [];
+  const right = [];
+  let similarity = 0;
+  const frequency = {};
+
+  data.forEach((el) => {
+    const [a, b] = el.split('   ');
+    left.push(Number(a));
+    right.push(Number(b));
+  });
+
+  left.sort((a, b) => a - b);
+  right.sort((a, b) => a - b);
+
+  for (const num of right) {
+    frequency[num] = (frequency[num] || 0) + 1;
+  }
+
+  left.forEach((el) => {
+    if (frequency[el]) {
+      similarity += frequency[el] * el;
+    }
+  });
+
+  return similarity;
 }
 
 await runSolution(day1b);
