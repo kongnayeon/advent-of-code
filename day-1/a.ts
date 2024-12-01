9f25aaae6d81425a02b259b3abf221a4edc10a85
kangnayeon <seoultownroad@naver.com>
2024-12-01 14:49:02 +0900

    day-1-a

--- day-1/a.ts
+++ day-1/a.ts
@@ -2,8 +2,19 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day1a(data: string[]) {
-  console.log(data);
-  return 0;
+  const left = [];
+  const right = [];
+
+  data.forEach((el) => {
+    const [a, b] = el.split('   ');
+    left.push(Number(a));
+    right.push(Number(b));
+  });
+
+  left.sort((a, b) => a - b);
+  right.sort((a, b) => a - b);
+
+  return left.reduce((sum, el, index) => sum + Math.abs(el - right[index]), 0);
 }
 
 await runSolution(day1a);
--- utils.ts
+++ utils.ts
@@ -9,9 +9,11 @@ export async function runSolution(solution: (data: string[]) => any) {
 }
 
 export async function readData() {
-  const [_, fullPath, dataSet] = process.argv as
-    | [string, string, string]
-    | [string, string];
+  const [_, fullPath, ...args] = process.argv;
+
+  const dataSetArg = args.find((arg) => arg.startsWith('--data='));
+  const dataSet = dataSetArg ? dataSetArg.split('=')[1] : undefined;
+
   const puzzle = fullPath.split('/').slice(-2).join('/');
   const [day, part] = puzzle
     .split('/')
@@ -24,4 +26,3 @@ export async function readData() {
 function createFileName(day: number, part: 'a' | 'b', dataSet?: string) {
   return join(`day-${day}`, `${part}.data${dataSet ? `.${dataSet}` : ''}.txt`);
 }
-
