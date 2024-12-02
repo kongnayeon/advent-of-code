de1bbb8b9b4c7f7ebf5fb9d92fe3ac238b67709b
kangnayeon <seoultownroad@naver.com>
2024-12-02 15:43:56 +0900

    day-2-a

--- day-2/a.ts
+++ day-2/a.ts
@@ -2,8 +2,38 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day2a(data: string[]) {
-  console.log(data);
-  return 0;
+  let safe = 0;
+
+  for (const el of data) {
+    const arr = el.split(' ').map(Number);
+    let curSort: 'asc' | 'desc' | null = null;
+    let isSafe = true;
+
+    for (let i = 1; i < arr.length; i++) {
+      const difference = arr[i] - arr[i - 1];
+
+      // 방향 설정
+      if (curSort === null) {
+        curSort = difference > 0 ? 'asc' : 'desc';
+      }
+
+      // 방향 일관성 및 차이 값 확인
+      if (
+        (curSort === 'asc' && difference <= 0) ||
+        (curSort === 'desc' && difference >= 0) ||
+        Math.abs(difference) > 3
+      ) {
+        isSafe = false;
+        break;
+      }
+    }
+
+    if (isSafe) {
+      safe++;
+    }
+  }
+
+  return safe;
 }
 
 await runSolution(day2a);
