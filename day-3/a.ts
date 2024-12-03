f0e213e78eab15a5d2cb2db1d498e2e7154cef73
kangnayeon <seoultownroad@naver.com>
2024-12-03 17:10:37 +0900

    day-3-a

--- day-3/a.ts
+++ day-3/a.ts
@@ -1,9 +1,22 @@
 import { runSolution } from '../utils.ts';
 
+// 정규식 활용 문제
+
 /** provide your solution as the return of this function */
 export async function day3a(data: string[]) {
-  console.log(data);
-  return 0;
+  const regex = /mul\(\d{1,3},\s*\d{1,3}\)/g;
+  const matches = data[0].match(regex);
+  let result = 0;
+
+  matches.forEach((el) => {
+    let removeLeft = el.replace('mul(', '');
+    let str = removeLeft.replace(')', '');
+
+    const [a, b] = str.split(',').map(Number);
+    result += a * b;
+  });
+
+  return result;
 }
 
 await runSolution(day3a);
