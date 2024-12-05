ffb7694e2830886caf624c9ffb8313d26b127513
kangnayeon <seoultownroad@naver.com>
2024-12-05 23:18:24 +0900

    day-5-b

--- day-5/b.ts
+++ day-5/b.ts
@@ -2,8 +2,50 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day5b(data: string[]) {
-  console.log(data);
-  return 0;
+  const separatorIndex = data.indexOf('');
+  const orderingArr = data.slice(0, separatorIndex);
+  const updateArr = data.slice(separatorIndex + 1);
+  let result = 0;
+
+  const orderMap = {};
+  orderingArr.forEach((order) => {
+    const [from, to] = order.split('|').map(Number);
+    if (!orderMap[from]) orderMap[from] = new Set();
+    orderMap[from].add(to);
+  });
+
+  updateArr.forEach((el) => {
+    const pages = el.split(',').map(Number);
+
+    let isValid = true;
+
+    for (let i = 1; i < pages.length; i++) {
+      if (orderMap[pages[i]] && orderMap[pages[i]].has(pages[i - 1])) {
+        isValid = false;
+        continue;
+      }
+    }
+
+    if (!isValid) {
+      pages.sort((a, b) => {
+        if (orderMap[b] && orderMap[b].has(a)) {
+          return 1;
+        }
+
+        if (orderMap[a] && orderMap[a].has(b)) {
+          return -1;
+        }
+
+        return 0;
+      });
+
+      const middlePage = pages[Math.floor(pages.length / 2)];
+
+      result += middlePage;
+    }
+  });
+
+  return result;
 }
 
 await runSolution(day5b);
